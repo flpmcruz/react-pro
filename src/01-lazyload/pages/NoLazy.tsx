288423abd6e7de66519641157acfbfa9dcca164e
flpmcruz <flpmireles@gmail.com>
2023-05-13 22:21:55 -0500

    lazy

--- src/01-lazyload/pages/NoLazy.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export const NoLazy = () => {
-  return (
-    <div>NoLazy</div>
-  )
-}
-
-export default NoLazy
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,11 @@
 import { lazy, LazyExoticComponent } from "react";
-import { NoLazy } from "../01-lazyload/pages/NoLazy";
 
 // Poniendo /* webpackChunkName: "LazyPage1" */ se puede cambiar el nombre del chunk
-// const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */"../01-lazyload/pages/LazyPage1"));
-// const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */"../01-lazyload/pages/LazyPage2"));
-// const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */"../01-lazyload/pages/LazyPage3"));
+const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */"../01-lazyload/pages/LazyPage1"));
+const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */"../01-lazyload/pages/LazyPage2"));
+const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */"../01-lazyload/pages/LazyPage3"));
 
 // Los componentes tienen que estar exportados como default
-const LazyLayout = lazy(() => import(/* webpackChunkName: "LazyLayout" */"../01-lazyload/layout/LazyLayout"));
 
 type JSXComponent = () => JSX.Element;
 
@@ -20,33 +18,21 @@ interface Route {
 
 export const routes: Route[] = [
   {
-    path: "/lazy-layout/*", // * para que coja cualquier cosa que venga después
-    to: "/lazy-layout",
-    Component: LazyLayout,
-    name: "Dashboard",
+    to: "/lazy1",
+    path: "/",
+    Component: Lazy1,
+    name: "Lazy-1",
   },
   {
-    path: "no-lazy",
-    to: "/no-lazy",
-    Component: NoLazy,
-    name: "No Lazy",
-  }
-  // {
-  //   to: "/lazy1",
-  //   path: "/",
-  //   Component: Lazy1,
-  //   name: "Lazy-1",
-  // },
-  // {
-  //   to: "/lazy2",
-  //   path: "/lazy2",
-  //   Component: Lazy2,
-  //   name: "Lazy-2",
-  // },
-  // {
-  //   to: "/lazy3",
-  //   path: "/lazy3",
-  //   Component: Lazy3,
-  //   name: "Lazy-3",
-  // },
+    to: "/lazy2",
+    path: "/lazy2",
+    Component: Lazy2,
+    name: "Lazy-2",
+  },
+  {
+    to: "/lazy3",
+    path: "/lazy3",
+    Component: Lazy3,
+    name: "Lazy-3",
+  },
 ];
